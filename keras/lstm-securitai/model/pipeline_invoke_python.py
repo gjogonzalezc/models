9c7a45dceca646dacc7171fe0c6fe97419f11dc5
cfregly <chris@fregly.com>
2018-09-10 04:24:33 +0000

    updated invoke to match existing predict example

--- keras/lstm-securitai/model/pipeline_invoke_python.py
+++ keras/lstm-securitai/model/pipeline_invoke_python.py
@@ -1,5 +1,7 @@
+import io
 import os
 import numpy as np
+import pandas
 import json
 import logging                                                 #<== Optional.  Log to console, file, kafka
 from pipeline_monitor import prometheus_monitor as monitor     #<== Optional.  Monitor runtime metrics
@@ -7,7 +9,10 @@ from pipeline_logger import log
 
 import tensorflow as tf
 from tensorflow.contrib import predictor
-from keras.models import load_model
+from keras.models import Sequential, load_model
+from keras.preprocessing import sequence
+from keras.preprocessing.text import Tokenizer
+from collections import OrderedDict
 
 _logger = logging.getLogger('pipeline-logger')
 _logger.setLevel(logging.INFO)
@@ -24,6 +29,7 @@ _labels = {                                                    #<== Optional.  U
            'runtime': 'python',
            'chip': 'cpu',
           }
+
 
 def _initialize_upon_import():                                 #<== Optional.  Called once upon server startup
     ''' Initialize / Restore Model Object.
@@ -46,7 +52,7 @@ def invoke(request):                                           #<== Required.  C
         transformed_request = _transform_request(request)      #<== Optional.  Transform input (json) into TensorFlow (tensor)
 
     with monitor(labels=_labels, name="invoke"):               #<== Optional.  Calls _model.predict()
-        response = _model.predict(request)
+        response = _model.predict(transformed_request)
 
     with monitor(labels=_labels, name="transform_response"):   #<== Optional.  Transform TensorFlow (tensor) into output (json)
         transformed_response = _transform_response(response)
@@ -56,19 +62,34 @@ def invoke(request):                                           #<== Required.  C
 
 def _transform_request(request):
     request_str = request.decode('utf-8')
-    request_json = json.loads(request_str)
-    #print(request_json)
-    return request_json
+
+    # tokenize the csv request and create json
+    X = pandas.read_csv(io.StringIO(request_str), engine='python', quotechar='|', header=None).values[:,0]
+    for index, item in enumerate(X):
+        reqJson = json.loads(item, object_pairs_hook=OrderedDict)
+        del reqJson['http']['timestamp']
+        del reqJson['http']['headers']
+        del reqJson['http']['source']
+        del reqJson['http']['route']
+        del reqJson['http']['responsePayload']
+        X[index] = json.dumps(reqJson, separators=(',', ':'))
+
+    tokenizer = Tokenizer(filters='\t\n', char_level=True)
+    tokenizer.fit_on_texts(X)
+    # this used to be [log_entry]
+    seq = tokenizer.texts_to_sequences([request_str])
+    max_log_length = 1024
+    log_entry_processed = sequence.pad_sequences(seq, maxlen=max_log_length)
+
+    return log_entry_processed 
 
 
 def _transform_response(response):
-    return response
-    #return json.dumps({"classes": response['classes'].tolist(),
-    #                   "probabilities": response['probabilities'].tolist(),
-    #                  })
+    return response[0]
+
 
 if __name__ == '__main__':
-    with open('./pipeline_test_request.json', 'rb') as fb:
+    with open('./pipeline_test_request.csv', 'rb') as fb:
         request_bytes = fb.read()
         response_bytes = invoke(request_bytes)
         print(response_bytes)
