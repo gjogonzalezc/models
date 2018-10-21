c5008c2cb9d1114b649bcb0982a5d019718f86d5
cfregly <chris@fregly.com>
2018-10-20 20:25:01 -0500

    udpated

--- spark/mnist/pipeline_invoke.scala
+++ spark/mnist/pipeline_invoke.scala
@@ -4,7 +4,7 @@ import ml.combust.mleap.runtime.MleapSupport._
 import ml.combust.mleap.runtime.serialization.FrameReader
 
 import resource._
-
+// https://github.com/combust/mleap/pull/286
 import ml.combust.mleap.runtime.MleapSupport
 import ml.combust.bundle.BundleFile
 
@@ -14,30 +14,34 @@ import resource._
 import ml.combust.bundle.BundleFile
 import org.apache.spark.ml.bundle.SparkBundleContext
 
-import ml.combust.mleap.runtime.MleapSupport._
 import ml.combust.mleap.runtime.MleapContext.defaultContext
 import java.io.File
 
 import org.apache.spark.ml.mleap.SparkUtil
-
+import ml.combust.mleap.runtime.frame.FrameBuilder
+import ml.combust.mleap.runtime.frame.DefaultLeapFrame
 
 object pipeline_invoke {
 
-def main(args: Array[String]): Unit = {
-  // load the Spark pipeline we saved in the previous section
-  // There is a wonky limitation of BundleFile that requires
-  //   this filename parameter to start at the /root/directory
-  val mleapPipeline = (for(bf <- managed(BundleFile("jar:file:/tmp/mnist-spark-pipeline.zip"))) yield {
+  def main(args: Array[String]): Unit = {
+    // load the Spark pipeline we saved in the previous section
+    // There is a wonky limitation of BundleFile that requires
+    //   this filename parameter to start at the /root/directory
+    val mleapPipeline = (for(bf <- managed(BundleFile("jar:file:/tmp/mnist-spark-pipeline.zip"))) yield {
       bf.loadMleapBundle().get.root
     }).tried.get
 
-  val s = scala.io.Source.fromFile("pipeline_test_request.json").mkString
+    val s = scala.io.Source.fromFile("pipeline_test_request.json").mkString
+    val bytes = s.getBytes("UTF-8")
 
-  val bytes = s.getBytes("UTF-8")
-  val frame = FrameReader("ml.combust.mleap.json").fromBytes(bytes)
+    println(s)
+    println(bytes)
 
-  // transform the dataframe using our pipeline
-  val frame2 = mleapPipeline.transform(frame).get
-  val data = frame2.dataset
-}
+    val frame = FrameReader("ml.combust.mleap.json").fromBytes(bytes)
+
+    val transformed_frame = mleapPipeline.transform(frame.get)
+    print(transformed_frame)
+    val data = transformed_frame.get.dataset
+    print(data)
+  }
 }
